4c3a71d2b8a1e5fed043166616f935d36e5a3535
sakurai00123 <zero2zero3zero4@gmail.com>
2020-07-13 14:25:17 +0900

    ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'logins#index'
+  get 'logins/index'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
