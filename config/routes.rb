a517ad44ed3acfdb06c6a84afcc93cb059369823
sakurai00123 <zero2zero3zero4@gmail.com>
2020-07-13 15:30:35 +0900

    routes修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'calenders/index'
   root to: 'logins#index'
   get 'logins/index'
+  resources :calenders
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
