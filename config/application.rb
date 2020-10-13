1b45ede4f5dc213edd25d588aa6f81895f01b8dc
sakurai00123 <zero2zero3zero4@gmail.com>
2020-10-14 00:51:17 +0900

    ログイン画面、登録画面CSS

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module BugsCookthings
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    # デフォルトのlocaleを日本語(:ja)にする
+    config.i18n.default_locale = :ja
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
