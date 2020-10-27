182b8ef098cc6219bf3f14c79327dc141b323225
sakurai00123 <zero2zero3zero4@gmail.com>
2020-10-27 22:34:49 +0900

    session切れ対応

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -106,12 +106,6 @@ class RecipesController < ApplicationController
   end
 
   private
-    # ログイン中か確認
-    def logged_in_user 
-      unless logged_in?
-        redirect_to login_url
-      end
-    end
 
   def recipe_param
     params.require(:recipe).permit(
